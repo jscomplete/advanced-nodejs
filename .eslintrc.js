0f5ed6d6ca2e8b9a2e7e3180cb8fcd194c8f3288
Samer Buna <samer.buna@gmail.com>
2016-10-10 09:22:26 -0700

    Who let the modules out

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "parser": 'babel-eslint',
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": ["eslint:recommended"],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true
+    },
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": ["error", 2],
+    "linebreak-style": ["error","unix"],
+    "quotes": ["error","single"],
+    "semi": ["error","always"],
+    "no-console": ["off"]
+  }
+};
