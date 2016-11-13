5f2ecfbb205a015a3b1b90b4e5c59c129694a4dd
Samer Buna <samer.buna@gmail.com>
2016-11-13 15:52:36 -0800

    Examples for 4.4

---
+++ 4.4/dns.js
@@ -0,0 +1,17 @@
+const dns = require('dns'); // name -- addresses
+
+// dns.lookup('pluralsight.com', (err, address) => {
+//   console.log(address);
+// });
+
+// dns.resolve4('pluralsight.com', (err, address) => {
+//   console.log(address);
+// });
+
+// dns.resolveMx('pluralsight.com', (err, address) => {
+//   console.log(address);
+// });
+
+// dns.reverse('35.161.75.227', (err, hostnames) => {
+//   console.log(hostnames);
+// });
