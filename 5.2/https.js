d335d73b119604d41a7fcc97ac8bde0d59de30ad
Samer Buna <samer.buna@gmail.com>
2016-11-14 12:20:36 -0800

    Examples for 5.2

---
+++ 5.2/https.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const server = require('https')
+  .createServer({
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem'),
+  });
+
+server.on('request', (req, res) => {
+  res.writeHead(200, { 'content-type': 'text/plain' });
+  res.end('Hello world\n');
+});
+
+server.listen(443);
