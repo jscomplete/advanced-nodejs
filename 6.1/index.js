8cad98b87413ff4e340cbf42171845d93ff2055c
Samer Buna <samer.buna@gmail.com>
2016-11-28 13:30:59 -0800

    Examples for 6.1

---
+++ 6.1/index.js
@@ -0,0 +1,21 @@
+const os = require('os');
+
+/*
+os.EOL
+os.arch()
+os.constants
+os.cpus()
+os.endianness()
+os.freemem()
+os.homedir()
+os.hostname()
+os.loadavg()
+os.networkInterfaces()
+os.platform()
+os.release()
+os.tmpdir()
+os.totalmem()
+os.type()
+os.uptime()
+os.userInfo()
+*/
